3abd900cab6deb6e1615b17002f77efa7e2cfc19
cielavenir <cielartisan@gmail.com>
2017-06-15 17:18:24 +0900

    Fixed build script

--- deps/build.jl
+++ deps/build.jl
@@ -5,5 +5,5 @@ liblzma = library_dependency("liblzma",aliases=["liblzma-5"])
 if is_windows()
 	using WinRPM
 	provides(WinRPM.RPM,"liblzma5",liblzma,os = :Windows)
-	@BinDeps.install Dict(:liblzma=>:liblzma)
 end
+@BinDeps.install Dict(:liblzma=>:liblzma)
